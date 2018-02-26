aa80e9f898237169c30f3ec119f28e177db19720
OlexandrBlanar <olexandrblanar@gmail.com>
2018-02-26 00:38:11 -0500

    edit style

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  title: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.title = 'travel helper';
   }
 
 }
