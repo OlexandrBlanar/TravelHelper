5d939cf3ce24165c3a74ecd64c7e9304527bfa80
unknown <C:\Users\elektro\AppData\Roaming\The Bat!>
2018-03-29 16:37:10 +0300

    edit mapService

--- src/app/map/info-place/info-place.service.ts
+++ src/app/map/info-place/info-place.service.ts
@@ -10,8 +10,9 @@ export class InfoPlaceService {
 
   getWikiInfo(placeName: string) {
     const url = this.searchUrl(placeName, wikiUrl);
+    console.log(url);
     this.http.jsonp(url, 'callback')
-      .map((response: Response) => response[1])
+      // .map((response: Response) => response[1])
       // .map((results: any[]) => results.map((result) => console.log(result)))
       .subscribe(results => console.log(results));
 
@@ -19,9 +20,17 @@ export class InfoPlaceService {
 
   searchUrl(term: string, base: string): string {
     const params = new HttpParams()
-      .append('action', 'opensearch')
-      .append('search', term)
-      .append('format', 'json');
+      .append('action', 'query')
+      .append('format', 'json')
+      .append('generator', 'search')
+      .append('gsrsearch', term)
+      .append('gsrnamespace', '0')
+      .append('gsrlimit', '10')
+      .append('prop', 'extracts')
+      .append('exchars', '200')
+      .append('exlimit', 'max')
+      .append('explaintext', 'true')
+      .append('exintro', 'true');
     return `${base}?${params.toString()}`;
   }
 
